0ae0d9fa6df1a73f1e43c7a42b7ed4c36f28e988
Mahesh <maheshudara244@gmail.com>
2021-02-26 23:09:22 +0530

    AWS test

--- src/main/java/com/company/Hello.java
+++ src/main/java/com/company/Hello.java
@@ -1,8 +1,9 @@
 package com.company;
 
+
+
 public class Hello {
-    public static String main(String[] args) {
-        System.out.println("Hello, World!");
-        return "Hello, World!";
+    public String Hello(){
+        return "Hello";
     }
 }
